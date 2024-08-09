64e113d4a3abfd39c420c150fa2fa7d51239e95b
Kai <90664464+iwataTMC@users.noreply.github.com>
2024-08-09 20:54:51 +0900

    Update q4-1.py

--- q4-1.py
+++ q4-1.py
@@ -1,4 +1,12 @@
-def hash2(time_str):
-    hours, minutes, seconds = map(int, time_str.split(":"))
-    total_seconds = hours * 3600 + minutes * 60 + seconds
-    return total_seconds % 36
+def hash1(s):
+    if not s:
+        return 0
+
+    first_char = s[0]
+    if first_char in "abcde":
+        # a, b, c, d, eで始まる文字列は0-8のバケットに割り当て
+        return (ord(first_char) - ord("a")) * 2 % 9
+    else:
+        # それ以外の文字で始まる文字列は9-11のバケットに割り当て
+        return 9 + (ord(first_char) - ord("f")) % 3
+
